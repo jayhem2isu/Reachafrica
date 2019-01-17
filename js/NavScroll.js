8d1b0ceaf256f968b2ce93aa953dc7990203921c
Isu <jayhem2isu@gmail.com>
2019-01-17 12:42:03 +0100

    Website upload

---
+++ js/NavScroll.js
@@ -0,0 +1,11 @@
+var myNav = document.getElementById('myTopnav');
+window.onscroll = function () {
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    }
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
